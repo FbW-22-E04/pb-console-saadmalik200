d109ddc35c45c6cad757adb0d159e9ba05ab2985
Saad Malik <saad_malik200@hotmail.com>
2022-06-29 23:19:46 +0200

    finished exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,19 @@
 // write your code in this file
+console.log("This is an exercise in console logging");
+
+const exercise = "This is the value.";
+console.log(exercise);
+
+const firstName = "saad";
+const lastName = "malik";
+const age = 31;
+const city = "Bremen";
+const job = "student";
+console.log(firstName, lastName, age, city, job);
+console.log(
+  firstName,
+  lastName,
+  "is a " + age,
+  "years old " + job,
+  "Who lives in " + city
+);
